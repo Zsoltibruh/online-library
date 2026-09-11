63eef639b79fc6967a729eaff596da32bea74f40
Zsoltibruh <savanyazsolt69@gmail.com>
2026-09-11 18:51:30 +0200

    feat: Add scheduled task for checking 2 month old reservations and setting their status to "lost".

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Enums\ReservationStatus;
 use App\Mail\ReservationReminder;
+use App\Models\LostBook;
 use App\Models\Reservation;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schedule;
 
@@ -26,3 +29,29 @@ Schedule::call(function () {
         }
     }
 })->name('Send reminders 3 days before due date')->daily();
+
+Schedule::call(function () {
+    $lostDate = now()->subMonths(2);
+
+    $reservations = Reservation::with(['user:id', 'book:id,count'])
+        ->whereNull('return_date')
+        ->whereNot('status', '=', ReservationStatus::Lost)
+        ->where('due_date', '<=', $lostDate)
+        ->get();
+
+    foreach ($reservations as $reservation) {
+        DB::transaction(function () use ($reservation) {
+            $reservation->book->decrement('count');
+
+            $reservation->update([
+                'status' => ReservationStatus::Lost,
+            ]);
+
+            LostBook::create([
+                'reservation_id' => $reservation->id,
+                'logger' => 'System',
+                'log_date' => now(),
+            ]);
+        });
+    }
+})->name('Mark 2 month old reservations lost')->daily();
